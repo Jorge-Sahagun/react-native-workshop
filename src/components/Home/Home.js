2585cced2c1d69cc6a8ca1aab7d7f960c2597fa4
Charlie <charlie@wizeline.com>
2019-01-21 14:55:38 -0600

    add components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
